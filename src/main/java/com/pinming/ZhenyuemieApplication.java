5a09e1261bd178fc31614addbb9974031ebd0222
admin <2225913091@qq.com>
2019-08-01 14:36:16 +0800

    Initial commit

---
+++ src/main/java/com/pinming/ZhenyuemieApplication.java
@@ -0,0 +1,13 @@
+package com.pinming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhenyuemieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhenyuemieApplication.class, args);
+    }
+
+}
